dcd2ec3c7faa1af71b17862d370c70365f150278
xuanthanh112 <139263402+xuanthanh112@users.noreply.github.com>
2023-10-30 22:07:27 +0700

    codelabweek2
    em gửi a bài tuần 2

---
+++ codelabweek2/java/DigitalVideoDisc.java
@@ -0,0 +1,50 @@
+public class DigitalVideoDisc {
+    // khai bao cac thuoc tinh
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+// getter setter
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+//khoi tao cac doi tuong dvd
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this(title);
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this(title, category, cost);
+        this.director = director;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this(title, category, director, cost);
+        this.length = length;
+    }
+
+
+}
